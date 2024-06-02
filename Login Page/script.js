bb117ed51c4658afe2a9920e07286b6fa792ac23
Yoga-Syahputra <yogasyahputrauzumaki@gmail.com>
2024-06-02 23:13:50 +0700

    Co-authored-by: Taufik-Hidayat-Sianturi <Taufik-Hidayat-Sianturi@users.noreply.github.com> Co-authored-by: kelvin-amzani-pasha <kelvin-amzani-pasha@users.noreply.github.com> Co-authored-by: SfqHaikal <SfqHaikal@users.noreply.github.com> Co-authored-by: syiaruddin <syiaruddin@users.noreply.github.com>

---
+++ Login Page/script.js
@@ -0,0 +1,13 @@
+function togglePasswordVisibility() {
+    const passwordInput = document.getElementById('password');
+    const toggleIcon = document.querySelector('.toggle-password i');
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        toggleIcon.classList.remove('fa-eye');
+        toggleIcon.classList.add('fa-eye-slash');
+    } else {
+        passwordInput.type = 'password';
+        toggleIcon.classList.remove('fa-eye-slash');
+        toggleIcon.classList.add('fa-eye');
+    }
+}
